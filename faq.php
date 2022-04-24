18411103f1e2aa08769af775ee45329b0f654293
gaychin <gcyap1227@gmail.com>
2022-04-24 19:54:32 +0800

    changes

--- faq.php
+++ faq.php
@@ -8,6 +8,11 @@ use PHPMailer\PHPMailer\Exception;
 //Load Composer's autoloader
 require 'vendor/autoload.php';
 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$pw = $_ENV['EMAIL_PASSWORD'];
+
 //Create an instance; passing `true` enables exceptions
 $mail = new PHPMailer(true);
 
@@ -54,7 +59,7 @@ if (isset($_POST['send-email'])) {
             $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
             $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
             $mail->Username   = 'gcyap1227@gmail.com';                     //SMTP username
-            $mail->Password   = 'DummyPassword';                               //SMTP password
+            $mail->Password   = $pw;                                    //SMTP password
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
             $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,6 +33,25 @@ class ComposerAutoloaderInitd9661d29a782df8affd1e32f5bfdee86
 
         $loader->register(true);
 
+        $includeFiles = \Composer\Autoload\ComposerStaticInitd9661d29a782df8affd1e32f5bfdee86::$files;
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequired9661d29a782df8affd1e32f5bfdee86($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+/**
+ * @param string $fileIdentifier
+ * @param string $file
+ * @return void
+ */
+function composerRequired9661d29a782df8affd1e32f5bfdee86($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+        require $file;
     }
 }
